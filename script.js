da7f979604f69fd6381f17c28f4fe2e4bc4d85b6
Mahariaman Bheta Kusuma <48098750+mahariaman@users.noreply.github.com>
2020-07-16 06:07:14 +0700

    update css width,btn hover,margin

---
+++ script.js
@@ -0,0 +1,38 @@
+function validate() {
+  let username = document.getElementById("usrname").value;
+  let psw = document.getElementById("password").value;
+
+  if (username === "" && psw === "") {
+    alert("di isi dulu cok")
+  } else {
+    if (username === "mahariaman" && psw === "123") {
+      alert("welcome to my channel");
+    } else {
+      alert("jancok");
+    }
+  }
+}
+
+
+function register() {
+  let username = document.getElementById("usrname").value;
+  let psw = document.getElementById("psw").value;
+  let cpsw = document.getElementById("cpsw").value;
+  let nama = document.getElementById("nama").value;
+
+  let userRegex = /^[^\W_]{5,48}$/g;
+  let pswRegex = /^[^\W_]{5,48}$/g;
+  let resUsername = userRegex.test(username);
+  let resPassword = pswRegex.test(psw);
+
+  if (!resUsername) {
+    alert("Username minimal 5 karakter harus berupa huruf, angka dan tanpa simbol");
+  } else if (!resPassword) {
+    alert("password minimal 5 karakter harus berupa huruf, angka dan tanpa simbol");
+  } else if (cpsw !== psw) {
+    alert("password anda tidak cocok");
+  } else {
+    alert("Anda berhasil membuat akun baru");
+  }
+
+}
